b49c6e37ea17e62336904e13009c5598644347d9
doh <theshawns0n@gmail.com>
2020-10-09 20:42:54 -0400

    quick pseudo code

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
+//post a note.
+//save the note
+//edit a note
+//delete a note
+//write files
+//read file
+//send file
+//create routes
+
+
+
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });  
