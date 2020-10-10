80c2fb793140c48fc1f923060b0a3a644f186c09
doh <theshawns0n@gmail.com>
2020-10-09 21:00:33 -0400

    index and notes html routed

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(express.static(__dirname));
 //read file
 //send file
 //create routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+  app.get("/notes.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
 
 
 
