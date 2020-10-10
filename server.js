6e108b9a9d164c8e26689a3fe07718222ffdedc9
doh <theshawns0n@gmail.com>
2020-10-09 22:47:02 -0400

    body post to api

--- server.js
+++ server.js
@@ -5,28 +5,41 @@ const app = express();
 const PORT = process.env.PORT || 3060;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
-
-//post a note.
-//save the note
-//edit a note
-//delete a note
-//write files
-//read file
-//send file
-//create routes
+app.use(express.static('public'));
+//create initial routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+});
 
-  app.get("/notes.html", (req, res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+});
+//write files//send file//save the note//api route
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"))
+});
+
+app.get("/api/notes/:id", (req, res) => {
+    const savedNotes = json.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+//important last route?
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+//send body to api json . with id
+app.post("/api/notes", (req, res) => {
+    const savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    const id = (savedNotes.length).toString();
+    const addNote = req.body;
+    addNote.id = id;
+    savedNotes.push(addNote);
+    //write file to api
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+});
 
 
-
-
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });  
